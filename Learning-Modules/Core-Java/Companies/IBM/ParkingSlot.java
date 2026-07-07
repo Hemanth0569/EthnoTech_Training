17f3a8bf923fc3216d283ca0fdb5bdbb6aab43d8
Hemanth <hemanthkumarkuruvaa@gmail.com>
2026-07-07 14:52:21 +0530

    Add files via upload

---
+++ Learning-Modules/Core-Java/Companies/IBM/ParkingSlot.java
@@ -0,0 +1,15 @@
+package Companies.IBM;
+
+public class ParkingSlot {
+    public static void main(String[] args) {
+        int[] arr={1,0,0,0,0,1};
+
+    }
+    public static int parking(int[] arr){
+        int maxi=0;
+        int prev=-1;
+        return 0;
+
+
+    }
+}
