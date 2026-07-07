dd53067c2e64a16360c7bcfdb882b59f1e009b29
Hemanth <hemanthkumarkuruvaa@gmail.com>
2026-07-07 14:56:51 +0530

    Add files via upload

---
+++ Learning-Modules/Core-Java/Oops/Encapsulation/Encap.java
@@ -0,0 +1,30 @@
+package Oops.Encapsulation;
+
+    class AccessModi{
+        public int a = 10;
+        private int b = 20;
+        protected int c = 30;
+        int d = 30;
+
+        public int getB(){
+            return b;
+        }
+        public void setB(int b){
+            this.b=b;
+        }
+    }
+
+    class Access1 extends AccessModi{
+        void printData(){
+            System.out.println(a);
+            System.out.println(getB());
+            System.out.println(c);
+            System.out.println(d);
+        }
+    }
+public class Encap {
+    public static void main(String[] args) {
+        Access1 acc = new Access1();
+        acc.printData();
+    }
+}
